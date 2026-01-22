2e38878e11c2c2692a825b975f306631c118039c
Jedjedanko <jadejabagatangco2702@gmail.com>
2026-01-22 16:45:36 +0800

    push last

--- counter.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-export const counterSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
-  seq: { type: Number, default: 0 }
-});
